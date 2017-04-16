bbbff8673a32939235f6f1f54583a66fea0e96b5
zhenxianyimeng <1920405993@qq.com>
2017-04-16 21:31:48 +0800

    T002

--- src/com/xlc/chuji/T002.java
+++ src/com/xlc/chuji/T002.java
@@ -5,6 +5,38 @@ import java.util.Scanner;
 public class T002 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		int[] left = new int[N];
+		int[] right = new int[N];
+		for(int i=0; i<N; i++)
+			arr[i] = sc.nextInt();
+		left[0] = 1;
+		right[N-1] = 1;
+		int max;
+		for(int i=1; i<N; i++){
+			max = 1;
+			for(int j=0; j<i; j++){
+				if(arr[j] < arr[i])
+					max = max > left[j]+1 ? max : left[j]+1;
+			}
+			left[i] = max;
+		}
 		
+		for(int i=N-2; i>=0; i--){
+			max = 1;
+			for(int j=N-1; j>i; j--){
+				if(arr[j] < arr[i])
+					max = max>right[j]+1? max:right[j]+1;
+			}
+			right[i] = max;
+		}
+		int mmax = 0;
+		for(int i=0; i<N; i++){
+			if(left[i] + right[i] -1 >mmax)
+				mmax = left[i]+right[i]-1;
+		}
+		System.out.println(N-mmax);
+		sc.close();
 	}
 }
